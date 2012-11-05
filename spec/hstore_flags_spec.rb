501e16390d96b35b19b2927a19c0cee958a612b4
Zachery Hostens <zacheryph@gmail.com>
2012-11-05 17:06:46 -0600

    update specs to successfully run. no db interaction

--- spec/hstore_flags_spec.rb
+++ spec/hstore_flags_spec.rb
@@ -10,6 +10,7 @@ class UserNoScopes < ActiveRecord::Base
 end
 
 class UserMultiFlags < ActiveRecord::Base
+  self.table_name = "users"
   hstore_flags :fighter, :lover, :referee
   hstore_flags :drinker, :smoker, :bartender, field: "more_flags"
 end
@@ -24,15 +25,26 @@ describe HStoreFlags do
   end
 
   it "creates scopes without scopes: false" do
-    u = User.new(fighter: true)
-    u.save
-    User.fighter.count.should == 1
-    User.not_figher.count.should == 0
-    User.not_lover.count.should == 1
-    User.lover.count.should == 0
+    User.should respond_to(:fighter)
+    User.should respond_to(:not_fighter)
+    User.should respond_to(:lover)
+    User.should respond_to(:not_lover)
   end
 
   it "does not create scopes with scopes: false" do
+    UserNoScopes.should_not respond_to(:fighter)
+    UserNoScopes.should_not respond_to(:not_fighter)
+    UserNoScopes.should_not respond_to(:lover)
+    UserNoScopes.should_not respond_to(:not_lover)
   end
 
+  it "creates proper scopes when no field is defined" do
+    User.fighter.to_sql.should =~ /defined\(flags, 'fighter'\) IS TRUE/
+    User.not_fighter.to_sql.should =~ /defined\(flags, 'fighter'\) IS NOT TRUE/
+  end
+
+  it "creates proper scopes when field is defined" do
+    UserMultiFlags.drinker.to_sql.should =~ /defined\(more_flags, 'drinker'\) IS TRUE/
+    UserMultiFlags.not_drinker.to_sql.should =~ /defined\(more_flags, 'drinker'\) IS NOT TRUE/
+  end
 end
