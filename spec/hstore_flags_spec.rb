b07597f8ba3b64c590a2beea2b8fdb513d935842
Zachery Hostens <zacheryph@gmail.com>
2012-11-05 16:38:11 -0600

    initial specs. failing to save records currently

---
+++ spec/hstore_flags_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+class User < ActiveRecord::Base
+  include HStoreFlags
+
+  hstore_flags :fighter, :lover
+end
+
+class UserNoScopes < ActiveRecord::Base
+  include HStoreFlags
+
+  self.table_name = "users"
+  hstore_flags :fighter, :lover, :referee, scopes: false
+end
+
+class UserMultiFlags < ActiveRecord::Base
+  include HStoreFlags
+
+  hstore_flags :fighter, :lover, :referee
+  hstore_flags :drinker, :smoker, :bartender, field: "more_flags"
+end
+
+describe HStoreFlags do
+  it "creates accessors for flags" do
+    u = User.new(fighter: true)
+    u.fighter.should  == true
+    u.fighter?.should == true
+    u.lover.should    == false
+    u.lover?.should   == false
+  end
+
+  it "creates scopes without scopes: false" do
+    u = User.new(fighter: true)
+    u.save
+    User.fighter.count.should == 1
+    User.not_figher.count.should == 0
+    User.not_lover.count.should == 1
+    User.lover.count.should == 0
+  end
+
+  it "does not create scopes with scopes: false" do
+  end
+
+end
