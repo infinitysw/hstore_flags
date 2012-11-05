e82553caa6ba7ec5b26676c9e02b2faa665736cc
Zachery Hostens <zacheryph@gmail.com>
2012-11-05 16:37:54 -0600

    update spec helpers to properly create database table

--- spec/database.rb
+++ spec/database.rb
@@ -1,11 +1,17 @@
 require 'active_record'
 
 # connect
-ActiveRecord::Base.establish_connection(adapter: 'pg', database: 'testing')
+ActiveRecord::Base.establish_connection(
+  adapter: 'postgresql',
+  database: 'testing')
+
+ActiveRecord::Base.connection.execute("CREATE EXTENSION IF NOT EXISTS hstore")
 
 ActiveRecord::Schema.define(version: 1) do
+  execute "DROP TABLE IF EXISTS users"
+
   create_table :users do |t|
-    t.hstore :flags
-    t.hstore :more_flags
+    t.column :flags, :hstore
+    t.column :more_flags, :hstore
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,12 @@ if ENV['VERSION']
   gem 'activesupport', ENV['VERSION']
   gem 'activerecord',  ENV['VERSION']
 end
-$LOAD_PATH << 'lib'
-require 'hstore_flags'
-require 'timeout'
 
+$LOAD_PATH.unshift File.dirname(__FILE__)
+$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 require 'active_record'
+require 'hstore_flags'
+
 puts "Using ActiveRecord #{ActiveRecord::VERSION::STRING}"
 
 require File.expand_path('../database', __FILE__)
