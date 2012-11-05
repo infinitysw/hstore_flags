b57282659bd683e1c2709a949ddef07dc2f428c3
Zachery Hostens <zacheryph@gmail.com>
2012-11-05 10:25:40 -0600

    properly create flag scopes/accessor methods

--- lib/hstore_flags.rb
+++ lib/hstore_flags.rb
@@ -1,12 +1,22 @@
 require 'hstore_flags/version'
 require 'active_support'
 require 'active_support/version'
+require 'activerecord-postgres-hstore'
 
 module HStoreFlags
   TRUE_VALUES = [true, 1, '1', 't', 'T', 'true', 'TRUE']
 
   def self.included(base)
     base.extend(ClassMethods)
+
+    protected
+    def set_hstore_flag_field(field, flag, value)
+      if TRUE_VALUES.include?(value)
+        self[field] = (self[field] || {}).merge({flag => true})
+      else
+        self.destroy_keys(field, flag)
+      end
+    end
   end
 
   module ClassMethods
@@ -15,23 +25,14 @@ module HStoreFlags
       field = opts[:field] || "flags"
 
       args.each do |flag|
-        define_method("#{flag}")  {self[field][flag.to_s] == "true"}
-        define_method("#{flag}?") {self[field][flag.to_s] == "true"}
+        define_method("#{flag}")  {(self[field] || {})[flag.to_s] == "true"}
+        define_method("#{flag}?") {(self[field] || {})[flag.to_s] == "true"}
         define_method("#{flag}=") {|val| set_hstore_flag_field(field, flag, val)}
 
         unless opts[:scopes] == false
-          scope "#{field}",     where("defined(#{field}, '#{flag}') IS TRUE")
-          scope "not_#{field}", where("defined(#{field}, '#{flag}') IS NOT TRUE")
+          scope "#{flag}",     where("defined(#{field}, '#{flag}') IS TRUE")
+          scope "not_#{flag}", where("defined(#{field}, '#{flag}') IS NOT TRUE")
         end
-      end
-    end
-
-    private
-    def set_hstore_flag_field(field, flag, value)
-      if TRUE_VALUES.include?(value)
-        self[field] = self[field].merge({flag => true})
-      else
-        self.destroy_keys(field, flag)
       end
     end
   end
